c403e58484f10d3a95a7c5c8ef54323cf4755d50
hesam <hesam@ucla.edu>
2009-09-11 13:29:49 -0700

    ok

--- ESJava/src/polyglot/ext/esj/ast/ESJLogPredMethodDecl_c.java
+++ ESJava/src/polyglot/ext/esj/ast/ESJLogPredMethodDecl_c.java
@@ -39,37 +39,4 @@ public class ESJLogPredMethodDecl_c extends ESJMethodDecl_c
 	return isFallback;
     }
 
-    protected MethodDecl_c reconstruct(TypeNode returnType, List formals,
-				       List throwTypes, Block body,  
-				       List quantVarD, boolean isPredicate, boolean isFallback) {
-	if (returnType != this.returnType ||
-	    ! CollectionUtil.equals(formals, this.formals) ||
-	    ! CollectionUtil.equals(throwTypes, this.throwTypes) ||
-	    body != this.body) {
-	    ESJLogPredMethodDecl_c n = (ESJLogPredMethodDecl_c) copy();
-	    n.formals = formals; //TypedList.copyAndCheck(throwTypes, Formal.class, true);
-	    n.throwTypes = TypedList.copyAndCheck(throwTypes, TypeNode.class, true);
-	    n.quantVarD = TypedList.copyAndCheck(quantVarD, LocalDecl.class, true);
-	    n.isPredicate = isPredicate;
-	    n.isFallback = isFallback;
-	    n.body = body;
-	    return n;
-	}
-
-	return this;
-    }
-
-
-    // Visit the children of the method. 
-    public Node visitChildren(NodeVisitor v) {
-	TypeNode returnType = (TypeNode) visitChild(this.returnType, v);
-	List formals = visitList(this.formals, v);
-	//List quantVarD = visitList(this.quantVarD, v);
-	List throwTypes = visitList(this.throwTypes, v);
-	Block body = (Block) visitChild(this.body, v);
-	return reconstruct(returnType, formals, throwTypes, body, quantVarD, this.isPredicate, this.isFallback);
-    }
-
-
-
 }
